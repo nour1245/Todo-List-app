7b719c2cec79e57d07f12e03bfa4cd6c0d4a4d68
nour <aldennour62@gmail.com>
2022-08-23 12:41:22 +0200

    Initial commit

---
+++ ToDoList/TableViewController.swift
@@ -0,0 +1,125 @@
+//
+//  TableViewController.swift
+//  ToDoList
+//
+//  Created by nour on 8/21/22.
+//  Copyright © 2022 nour. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    var items = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.items = UserDefaults.standard.stringArray(forKey: "items") ?? []
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(DidTapAdd))
+        
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return items.count
+    }
+    
+    @objc func DidTapAdd(){
+        let alert = UIAlertController(title: "add", message: "add new item...", preferredStyle: .alert)
+        alert.addTextField{ field in
+            field.placeholder = "Enter your mission"
+            
+        }
+        alert.addAction(UIAlertAction(title: "cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Add", style: .default, handler: { (_) in
+            
+            if let field = alert.textFields?.first{
+                if let text = field.text , !text.isEmpty{
+                    DispatchQueue.main.async {
+                        self.items.append(text)
+                        var currentEntry = UserDefaults.standard.stringArray(forKey: "items") ?? []
+                        currentEntry.append(text)
+                        UserDefaults.standard.setValue(currentEntry, forKey: "items")
+                        self.tableView.reloadData()
+                        
+                    
+                    }
+                    
+                    
+                }
+            }
+            
+        }))
+        
+        present(alert , animated: true)
+        
+    }
+
+    
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            items.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 75
+    }
+    
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
